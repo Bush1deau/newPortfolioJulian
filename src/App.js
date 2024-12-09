d672f73aac4be83d00f632ba006d2a51c195b970
Bush1deau <juju.leroy47@gmail.com>
2024-12-09 21:11:01 +0100

    feat/langages add carousel with all langages, CV page done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import './index.css';
 import Cv from './Cv';
 import Realisations from './Realisations';
 import About from './About';
 import Contact from './Contact';
 
-const NavLink = ({ to, text, navLinkStyle, navLinkHoverStyle }) => (
+const NavLink = ({ to, text, navLinkStyle, navLinkHoverStyle, onClick }) => (
   <Link
     to={to}
     style={navLinkStyle}
@@ -26,13 +26,18 @@ const NavLink = ({ to, text, navLinkStyle, navLinkHoverStyle }) => (
       e.target.style.backgroundColor = '';
       e.target.style.color = navLinkStyle.color;
     }}
+    onClick={onClick} // Ferme le menu au clic
   >
     {text}
   </Link>
 );
 
-const App = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const Navigation = ({ menuOpen, setMenuOpen }) => {
+  const location = useLocation(); // Hook utilisé ici
+
+  useEffect(() => {
+    setMenuOpen(false); // Ferme le menu lors d'une navigation
+  }, [location, setMenuOpen]);
 
   const navLinkStyle = {
     textDecoration: 'none',
@@ -47,77 +52,64 @@ const App = () => {
     color: 'white',
   };
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 768) {
-        setMenuOpen(false);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
   return (
-    <Router>
-      <div>
-        {/* Navigation */}
-        <nav style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          padding: '10px',
-          backgroundColor: '#178ca4', // Turquoise
-          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-        }}>
-          <div className="nav-links">
-            <NavLink to="/" text="Accueil" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
-            <NavLink to="/cv" text="Mon CV" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
-            <NavLink to="/realisations" text="Mes réalisations" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
-            <NavLink to="/about" text="A propos de moi" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
-            <NavLink to="/contact" text="Contact" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
-          </div>
-          <button className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={toggleMenu}>
-            &#9776;
-          </button>
-        </nav>
-
-        {/* Burger Menu */}
-        <div className={`burger-nav ${menuOpen ? 'open' : ''}`}>
+    <>
+      <nav style={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '10px',
+        backgroundColor: '#178ca4', // Turquoise
+        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+      }}>
+        <div className="nav-links">
           <NavLink to="/" text="Accueil" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
           <NavLink to="/cv" text="Mon CV" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
           <NavLink to="/realisations" text="Mes réalisations" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
           <NavLink to="/about" text="A propos de moi" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
           <NavLink to="/contact" text="Contact" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} />
         </div>
+        <button className={`burger-menu ${menuOpen ? 'open' : ''}`} onClick={() => setMenuOpen(!menuOpen)}>
+          &#9776;
+        </button>
+      </nav>
 
-        {/* Routes */}
-        <Routes>
-          <Route path="/" element={
-            <div style={{ textAlign: 'center', marginTop: '10%' }}>
-              <h1 className="fade-in">Bienvenue sur le portfolio de <br /> Julian LEROY</h1>
-              <p style={{
-                fontSize: '1.5rem',
-                color: '#34495e', // Gris foncé
-                marginTop: '10px',
-              }}>
-                <i>Construisons ensemble l'avenir numérique !</i>
-              </p>
-            </div>
-          } />
-          <Route path="/cv" element={<Cv />} />
-          <Route path="/realisations" element={<Realisations />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+      {/* Burger Menu */}
+      <div className={`burger-nav ${menuOpen ? 'open' : ''}`}>
+        <NavLink to="/" text="Accueil" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} onClick={() => setMenuOpen(false)} />
+        <NavLink to="/cv" text="Mon CV" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} onClick={() => setMenuOpen(false)} />
+        <NavLink to="/realisations" text="Mes réalisations" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} onClick={() => setMenuOpen(false)} />
+        <NavLink to="/about" text="A propos de moi" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} onClick={() => setMenuOpen(false)} />
+        <NavLink to="/contact" text="Contact" navLinkStyle={navLinkStyle} navLinkHoverStyle={navLinkHoverStyle} onClick={() => setMenuOpen(false)} />
       </div>
+    </>
+  );
+};
+
+const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <Router>
+      <Navigation menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Routes>
+        <Route path="/" element={
+          <div style={{ textAlign: 'center', marginTop: '10%' }}>
+            <h1 className="fade-in">Bienvenue sur le portfolio de <br /> Julian LEROY</h1>
+            <p style={{
+              fontSize: '1.5rem',
+              color: '#34495e', // Gris foncé
+              marginTop: '10px',
+            }}>
+              <i>Construisons ensemble l'avenir numérique !</i>
+            </p>
+          </div>
+        } />
+        <Route path="/cv" element={<Cv />} />
+        <Route path="/realisations" element={<Realisations />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 };
