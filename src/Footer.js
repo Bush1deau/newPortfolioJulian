abb4f7ee6d2030cb47b1e391b916342994b6afcd
Bush1deau <juju.leroy47@gmail.com>
2026-04-15 21:29:50 +0200

    fix: réduction de la taille du footer et de la distance des bulles

--- src/Footer.js
+++ src/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
               className="bubble"
               style={{
                 "--size": `${2 + Math.random() * 4}rem`,
-                "--distance": `${6 + Math.random() * 4}rem`,
+                "--distance": `${2 + Math.random() * 2}rem`,
                 "--position": `${-5 + Math.random() * 110}%`,
                 "--time": `${2 + Math.random() * 2}s`,
                 "--delay": `${-1 * (2 + Math.random() * 2)}s`,
